4cd6f16b6f53a2b9ed7c1381806ae47c1a2af935
Simon Baptiste <Baptiste.Simon@swisscom.com>
2020-02-28 15:46:10 +0100

    travis.py up

---
+++ sunneberg_project/settings/travis.py
@@ -0,0 +1,12 @@
+from . import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'travis_ci_test',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    },
+}
