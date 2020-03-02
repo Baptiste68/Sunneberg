f0a64d592d0e19ca81947cb146d00fb3724a650c
Simon Baptiste <Baptiste.Simon@swisscom.com>
2020-03-02 12:49:38 +0100

    Travis reconf

--- sunneberg_project/settings/travis.py
+++ sunneberg_project/settings/travis.py
@@ -2,7 +2,14 @@ from . import *
 
 DATABASES = {
     'default': {
-        'ENGINE':   'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'OPTIONS': {
+            'options': '-c search_path=django,sbschema,public'
+        },
+        'TEST': {
+            'NAME': 'test_sunneberg',
+            'options': '-c search_path=django,public'
+        },
         'USER': 'postgres',
         'NAME': 'sunneberg',
         'PASSWORD': '',
