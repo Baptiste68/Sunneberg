ebb0d78be17da12327ba146185ab398009404348
Simon Baptiste <Baptiste.Simon@swisscom.com>
2020-03-25 14:30:03 +0100

    email to local settings & Footer edit

--- sunneberg_project/settings/__init__.py
+++ sunneberg_project/settings/__init__.py
@@ -160,8 +160,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'sunneberg.proj@gmail.com'
-EMAIL_HOST_PASSWORD = 'P4nD109*'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 CONFIRM_UNSUB = 'http://127.0.0.1:8000/sunneberg/unsubconfirm'
 
---
+++ sunneberg_project/settings/settings_local.py
@@ -0,0 +1,8 @@
+from . import *
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'sunneberg.proj@gmail.com'
+EMAIL_HOST_PASSWORD = 'P4nD109*'
